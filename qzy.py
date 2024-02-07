40063f44c6eff9416fb2bcf748426a8816c753a9
Qu2358632966 <159237709+Qu2358632966@users.noreply.github.com>
2024-02-07 15:52:31 +0800

    Add files via upload

---
+++ qzy.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 15:26:35 2024
+
+@author: qu
+"""
+
+import streamlit as st
+st.title("你好,世界")
+st.markdown(
+    '''
+    # 你好\n
+    世界
+    '''
+    
+    
+    
+    
+    
+    )
